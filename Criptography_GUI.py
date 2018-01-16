5403ec96ef23354049a3c09c263dc4aefa681f75
TeijiW <35480877+TeijiW@users.noreply.github.com>
2018-01-16 19:58:12 -0300

    In Progress ( now without error )
    The error of " pyimage1 " has been resolved

--- Criptography_GUI.py
+++ Criptography_GUI.py
@@ -1,10 +1,6 @@
 from tkinter import *
 import tkinter.ttk as ttk
 
-
-#flat = ttk.Style()
-
-
 master = Tk()
 master.resizable(width=False, height=False)
 w = 400
@@ -13,13 +9,14 @@ sw = master.winfo_screenwidth()
 sh = master.winfo_screenheight()
 sw = int((sw/2) - (w/2))
 sh = int((sh/2) - (h/2))
+flat = ttk.Style()
 back = "#98979b"
 master.geometry("{}x{}+{}+{}".format(w,h,sw,sh))
 menu = Frame(master)
 encryptmenu = Frame(master)
 decryptmenu = Frame(master)
-imageencrypt = PhotoImage(file="C:\Python Projects\Criptography GUI\EncryptButton.png")
-imagedecrypt = PhotoImage(file="C:\Python Projects\Criptography GUI\DecryptButton.png")
+imageencrypt = PhotoImage(file="C:\Python Projects\Criptography GUI\EncryptButton.png", master=master)
+imagedecrypt = PhotoImage(file="C:\Python Projects\Criptography GUI\DecryptButton.png", master=master)
 master.configure(background=back)
 
 
