40587831479c2d78f4f91fb71dc839a39ac9f678
Teiji <35480877+TeijiW@users.noreply.github.com>
2018-01-21 02:49:09 -0300

    Main Script with GUI
    100% Works!

--- Criptography_GUI.py
+++ Criptography_GUI.py
@@ -1,8 +1,7 @@
 from tkinter import *
 from tkinter import font
 import tkinter.ttk as ttk
-
-
+import string
 
 master = Tk()
 master.resizable(width=False, height=False)
@@ -24,7 +23,55 @@ master.configure(background=back)
 menu.configure(bg=back)
 encryptmenu.configure(bg=back)
 decryptmenu.configure(bg=back)
+abclist = string.ascii_lowercase + string.ascii_lowercase
 
+def encryptsystem(): 
+    encryptedmessage.delete("1.0", END)
+    crypted = []
+    str1 = entrymessage.get("1.0", END+"-1c")
+    str1 = str1.lower() 
+    strlist = list(str1)
+    rot = int(rotwidget.get())
+    for x in strlist:
+        if x not in abclist:
+            crypted.append("#")
+        else:
+            n = abclist.find(x)
+            crypted.append(abclist[n+rot])
+    
+    str1 = "".join(crypted)
+    encryptedmessage.insert("1.0", str1)
+    
+def decryptsystem():
+    decryptedmessage.delete("1.0", END)
+    decrypted = []
+    str1 = entryencryptedmessage.get("1.0", END+"-1c")
+    strl = str1.lower()
+    strlist = list(str1)
+    cont = -1 
+    rot = -1
+    mastercont = 0
+    while True:
+        cont += 1
+        if cont == len(strlist):
+            cont = 0
+            rot -= 1
+            mastercont += 1
+            decrypted = "".join(decrypted)
+            decryptedmessage.insert("1.0", "Rotation {}: {} \n".format(mastercont, decrypted))
+            decrypted = []
+            if mastercont == 25:
+                break
+        m = strlist[cont]
+        n = abclist.find(m)
+        n = n+26
+        if m == "#" or m == " ":
+            decrypted.append(" ")
+        else:
+            decrypted.append(abclist[n+rot])
+    
+    
+    
 def restart():
     encryptmenu.grid_forget()
     decryptmenu.grid_forget()
@@ -44,6 +91,8 @@ def start():
     decryptbutton.pack(side=TOP)
     
 def encrypt():
+    global entrymessage, rotwidget, encryptedmessage
+    
     decryptmenu.grid_forget()
     menu.pack_forget()
 
@@ -58,17 +107,17 @@ def encrypt():
     entrymessage.grid(row=0, column=2, sticky=W)
     scrollbar2.config(command=entrymessage.yview)
     
-    okbutton = Button(encryptmenu, relief="ridge", font=secundaryfont, text="Encrypt!", bg=back, activebackground=back)
+    okbutton = Button(encryptmenu, relief="ridge", font=secundaryfont, text="Encrypt!", bg=back, activebackground=back, command=encryptsystem, width=8)
     okbutton.grid(row=1,column=1, sticky=S)
     
-    backbutton = Button(encryptmenu, relief="ridge", font=secundaryfont, text="<<< Back", bg=back, activebackground=back, command=restart)
+    backbutton = Button(encryptmenu, relief="ridge", font=secundaryfont, text="<<< Back", bg=back, activebackground=back, command=restart, width=8)
     backbutton.grid(row=3, column=1, sticky=N)
     
     labelmessage4 = Label(encryptmenu, relief="flat", font=secundaryfont, text="Rotations: ", bg=back, activebackground=back)
     labelmessage4.grid(row=2, column=2, sticky=W)
     
-    rotationoption = Spinbox(encryptmenu, relief="flat", font=entryfont, from_=1, to=26, increment=1, width=2)
-    rotationoption.grid(row=2, column=2, sticky=N)
+    rotwidget = Spinbox(encryptmenu, relief="flat", font=entryfont, from_=1, to=26, increment=1, width=2)
+    rotwidget.grid(row=2, column=2, sticky=N)
      
     labelmessage2 = Label(encryptmenu, text="Encrypted Message:", relief="flat", bg=back, activebackground=back, font=secundaryfont)
     labelmessage2.grid(row=4, column=1)
@@ -80,22 +129,36 @@ def encrypt():
     scrollbar1.config(command=encryptedmessage.yview)
     
 def decrypt():
+    global decryptedmessage, entryencryptedmessage
+    
     encryptmenu.grid_forget()
     menu.pack_forget() 
     
     decryptmenu.grid()
     
-    labelmessage3 = Label(decryptmenu, text="Encrypted Message: ", relief="flat", bg=back, activebackground=back, font=secundaryfont)
-    labelmessage3.grid(row=0, column=1) 
+    labelmessage3 = Label(decryptmenu, text="Encrypted Message:", relief="flat", bg=back, activebackground=back, font=secundaryfont)
+    labelmessage3.grid(row=0, column=1, sticky=W) 
     
-    entryencryptedmessage = Text(decryptmenu, relief="flat", font=entryfont, height=5, width=37)
+    scrollbar3 = Scrollbar(decryptmenu)
+    scrollbar3.grid(row=0, column=3, sticky=S+N)
+    entryencryptedmessage = Text(decryptmenu, relief="flat", font=entryfont, height=5, width=36, yscrollcommand=scrollbar3.set, wrap=WORD)
     entryencryptedmessage.grid(row=0, column=2, sticky=W)
+    scrollbar3.config(command=entryencryptedmessage.yview)
     
-    okbutton = Button(decryptmenu, relief="ridge", font=secundaryfont, text="Decrypt!", bg=back, activebackground=back)
-    okbutton.grid(row=1,column=1)
+    okbutton = Button(decryptmenu, relief="ridge", font=secundaryfont, text="Decrypt!", bg=back, activebackground=back, width=8, command=decryptsystem)
+    okbutton.grid(row=1,column=1, sticky=N)
     
-    backbutton = Button(decryptmenu, relief="ridge", font=secundaryfont, text="<<< Back", bg=back, activebackground=back, command=restart)
-    backbutton.grid(row=2, column=1)
+    backbutton = Button(decryptmenu, relief="ridge", font=secundaryfont, text="<<< Back", bg=back, activebackground=back, command=restart, width=8)
+    backbutton.grid(row=2, column=1, sticky=S)
+    
+    labelmessage5 = Label(decryptmenu, text="Decrypted Message: ", relief="flat", bg=back, activebackground=back, font=secundaryfont)
+    labelmessage5.grid(row=3, column=1)
+    
+    scrollbar4 = Scrollbar(decryptmenu)
+    scrollbar4.grid(row=3, column=3, sticky=S+N)
+    decryptedmessage = Text(decryptmenu, relief="flat", font=entryfont, height=5, width=36, yscrollcommand=scrollbar4.set, wrap=WORD)
+    decryptedmessage.grid(row=3, column=2, sticky=W)
+    scrollbar4.config(command=decryptedmessage.yview)
     
     
 start()     
